1d47d46d306ee88a469c002cc250d9c7efef01ef
Randy Steele <steelerx155@gmail.com>
2019-12-19 20:34:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,12 @@ class Dog
        dog
     end
     
-    def self.new_from_db
+   def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
       
   
 end
